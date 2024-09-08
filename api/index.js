5197f862f4e5759e1c8a36a7a8dc73d2c5a4f524
Correct Username <correct@example.com>
2024-09-08 21:14:57 +0530

    dgdfgc

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const CHUNK_SIZE = 1000; // Number of records per chunk
-const MAX_RECORDS = 30000;
+const MAX_RECORDS = 20000;
 
 
 
