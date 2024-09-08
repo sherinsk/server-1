beb5b973e846fdc97eeb1ce48d49fda17c97e2e6
Correct Username <correct@example.com>
2024-09-08 20:55:32 +0530

    klzdkgnlmsd,vx

--- api/index.js
+++ api/index.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-const CHUNK_SIZE = 10; // Number of records per chunk
-const MAX_RECORDS = 10;
+const CHUNK_SIZE = 1000; // Number of records per chunk
+const MAX_RECORDS = 200000;
 
 
 
